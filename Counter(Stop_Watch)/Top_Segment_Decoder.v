808c4922094f31675bb9f7f11067d5030a6ae342
kilee92 <82597294+kilee92@users.noreply.github.com>
2022-07-19 16:31:48 +0900

    Update Top_Segment_Decoder.v

--- Counter(Stop_Watch)/Top_Segment_Decoder.v
+++ Counter(Stop_Watch)/Top_Segment_Decoder.v
@@ -3,7 +3,18 @@
 //설계 목표: Counter를 이용한 분주기(prescaler)와 Finite State Machine (Moore)를 이용하여 스톱워치 설계
 //참고: 시스템은 50MHz의 기본주파수를 갖음
 
-module Top_Segment_decoder();
+module Top_Segment_decoder(
+    clk         ,
+    rst_n       ,
+    t_ms0       ,
+    t_ms1       ,
+    t_s0        ,
+    t_s1        ,
+    t_m0        ,
+    t_m1        ,
+    seg_sel     ,
+    o_seg            
+);
 
 input               clk         ;
 input               rst_n       ;
