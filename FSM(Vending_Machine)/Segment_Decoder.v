befe691a7b7b1b732a1583ae956dad4502093f59
kilee92 <82597294+kilee92@users.noreply.github.com>
2022-07-20 14:18:18 +0900

    Update Segment_Decoder.v

--- FSM(Vending_Machine)/Segment_Decoder.v
+++ FSM(Vending_Machine)/Segment_Decoder.v
@@ -13,13 +13,13 @@ input [1:0]         BCD_signal  ;
 output [7:0]        o_seg       ;
 
 //Combinational logic을 통한 output 출력 (Karno map 이용)
-assign o_seg[0] = (~i_seg[0]) | i_seg[1];
+assign o_seg[0] = ~BCD_signal[0] | ~BCD_signal[1];
 assign o_seg[1] = 1;
-assign o_seg[2] = i_seg[0] | (~i_seg[1]);
-assign o_seg[3] = (~i_seg[0]) | i_seg[1];
-assign o_seg[4] = ~i_seg[0];
-assign o_seg[5] = (~i_seg[0]) & (~i_seg[1]);
-assign o_seg[6] = i_seg[1];
+assign o_seg[2] = ~BCD_signal[0] | (~BCD_signal[1]);
+assign o_seg[3] = (~BCD_signal[0]) | ~BCD_signal[1];
+assign o_seg[4] = ~BCD_signal[0];
+assign o_seg[5] = (~BCD_signal[0]) & (~BCD_signal[1]);
+assign o_seg[6] = BCD_signal[1];
 assign o_seg[7] = 0;
 
 endmodule
