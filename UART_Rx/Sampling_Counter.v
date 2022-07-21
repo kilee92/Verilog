1a842d5b8fed454db311c1477bfc1203ab8b27e4
kilee92 <82597294+kilee92@users.noreply.github.com>
2022-07-21 16:58:27 +0900

    Update Sampling_Counter.v

--- UART_Rx/Sampling_Counter.v
+++ UART_Rx/Sampling_Counter.v
@@ -1,8 +1,9 @@
 //작성자: 이광일
 //설계명: UART(Rx)
 //설계 목표: Shift Register를 포함하는 비동기 통신 중 하나인 UART Tx Module 설계
-//참고: 데이터 전송 시점을 예측할 수 없기 때문에 1/Baud Rate보다 빠른 주기로 샘플링 하여 시작 비트가 수신되었는지 감지하는 기능을 포함해야함.
-//      신호선의 무결성을 보장할 수 없으므로, 데이터 비트의 중간 지점 3부분에서 데이터를 샘플링하여 가장 많이 샘플링된 데이터 값 2개를 저장해야함.
+//참고:
+//- 데이터 전송 시점을 예측할 수 없기 때문에 1/Baud Rate보다 빠른 주기로 샘플링 하여 시작 비트가 수신되었는지 감지하는 기능을 포함해야함.
+//- 신호선의 무결성을 보장할 수 없으므로, 데이터 비트의 중간 지점 3부분에서 데이터를 샘플링하여 가장 많이 샘플링된 데이터 값을 저장해야함.
 
 module Sampling_Counter
 #(
@@ -13,13 +14,13 @@ module Sampling_Counter
 (
     clk         ,
     rst_n       ,
-    sampling    ,                    
-)
+    sampling                        
+);
 
-input               clk         ,
-input               rst_n       ,
+input               clk         ;
+input               rst_n       ;
 
-output              sampling    ,
+output reg          sampling    ;
 
 reg [4:0] sampling_reg;
 
