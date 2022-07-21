5e4b15d49ef5503276fafb019e87036cec6316f6
kilee92 <82597294+kilee92@users.noreply.github.com>
2022-07-21 14:22:02 +0900

    Update FSM_Module_Rx.v

--- UART_Rx/FSM_Module_Rx.v
+++ UART_Rx/FSM_Module_Rx.v
@@ -1,15 +1,18 @@
 //작성자: 이광일
 //설계명: UART(Rx)
 //설계 목표: Shift Register를 포함하는 비동기 통신 중 하나인 UART Tx Module 설계
-//참고: 데이터 전송 시점을 예측할 수 없기 때문에 1/Baud Rate보다 빠른 주기로 샘플링 하여 시작 비트가 수신되었는지 감지하는 기능을 포함해야함.
-//      신호선의 무결성을 보장할 수 없으므로, 데이터 비트의 중간 지점 3부분에서 데이터를 샘플링하여 가장 많이 샘플링된 데이터 값 2개를 저장해야함.
+//참고:
+//- 데이터 전송 시점을 예측할 수 없기 때문에 1/Baud Rate보다 빠른 주기로 샘플링 하여 시작 비트가 수신되었는지 감지하는 기능을 포함해야함.
+//- 신호선의 무결성을 보장할 수 없으므로, 데이터 비트의 중간 지점 3부분에서 데이터를 샘플링하여 가장 많이 샘플링된 데이터 값을 저장해야함.
 
 module FSM_Module_Rx(
     clk                 ,
     rst_n               ,
     i_rx_d              ,
     sampling            ,
-    o_rx_d              ,
+    catch_bit           ,
+    catch_bit_cnt       ,
+    shift_rst           ,
     o_rx_complete       ,
     o_rx_error           
 );
@@ -19,14 +22,10 @@ input               rst_n               ;
 input               i_rx_d              ;
 input               sampling            ;
 
-o_rx_d              ;
-o_rx_complete       ;
-o_rx_error          ;
-
-
-[3:0] bit_cnt_reg;
-b0, b1, b2;
- catch_bit;
+output reg          catch_bit           ;
+output reg [3:0]    catch_bit_cnt       ;
+output              o_rx_complete       ;
+output              o_rx_error          ;
 
 reg [3:0] state, n_state;
 
@@ -56,8 +55,8 @@ end
 //State logic
 always @(state or i_rx_d or sampling or sample_cnt) begin //state, i_rx_d, sampling 신호가 바뀔 때에만 동작(clk와 상관(X))
     case(state)
-        IDLE: begin
-            if(sampling == 1 && i_rx_d == 0) //sampling하여 시작 비트(0)를 감지
+        IDLE: begin //sampling하여 시작 비트(0)를 감지하여 다음 state로 이동
+            if(sampling == 1 && i_rx_d == 0)
                 n_state = START_CHECK;
             else
                 n_state = IDLE;
@@ -74,10 +73,10 @@ always @(state or i_rx_d or sampling or sample_cnt) begin //state, i_rx_d, sampl
 
         SAMPLE_CNT_RST: n_state = F_WAIT; //sample_cnt 리셋
 
-        F_WAIT: begin //시작 비트(0) middle point에서 부터 data bit (middle point - 1) 까지 
+        F_WAIT: begin //시작 비트(0) middle point에서 부터 data bit (middle point - 2) 까지 
             if(sampling) begin
-                if(sample_cnt_reg == 15) //sample_cnt = 0 일 때 1번 flag // sample_cnt = 1(sample_cnt = 0 이 끝나는 순간) 1번 check 
-                    n_state = F_SAMPLE; // 다음 클럭에 state 변화 (다음 sampling이 아닌)
+                if(sample_cnt_reg == 14) 
+                    n_state = F_SAMPLE; // 다음 클럭에 state 변화 (다음 sampling이 아님)
                 else
                     n_state = F_WAIT;
             end else
@@ -86,9 +85,9 @@ always @(state or i_rx_d or sampling or sample_cnt) begin //state, i_rx_d, sampl
 
         F_SAMPLE: n_state = S_WAIT; //데이터 비트의 1번째 sample 저장
 
-        S_WAIT: begin //시작 비트(0) middle point에서 부터 data bit (middle point) 까지 
+        S_WAIT: begin //시작 비트(0) middle point에서 부터 data bit (middle point - 1) 까지 
             if(sampling_flag == 1) begin
-                if(sample_cnt_reg == 16)  // sample_cnt = 16 일때 16번 check가 끝나는 시점 -> middle point
+                if(sample_cnt_reg == 15) 
                     n_state = S_SAMPLE;
                 else
                     n_state = S_WAIT;
@@ -98,9 +97,9 @@ always @(state or i_rx_d or sampling or sample_cnt) begin //state, i_rx_d, sampl
 
         S_SAMPLE: n_state = T_WAIT; //데이터 비트의 2번째 sample 저장
 
-        T_WAIT: begin //시작 비트(0) middle point에서 부터 data bit (middle point + 1) 까지 
+        T_WAIT: begin //시작 비트(0) middle point에서 부터 data bit (middle point) 까지 
             if(sampling_flag == 1) begin
-                if(sample_cnt_reg == 17)
+                if(sample_cnt_reg == 16) //sample_cnt = 16 일때 16번 check가 끝나는 시점 -> middle point
                     n_state = T_SAMPLE;
                 else
                     n_state = T_WAIT;
@@ -108,17 +107,16 @@ always @(state or i_rx_d or sampling or sample_cnt) begin //state, i_rx_d, sampl
                 n_state = T_WAIT;
         end
 
-        T_SAMPLE: begin //데이터 비트의 3번째 sample 저장
-            if(bit_cnt_reg == 4'd8) // bit Counter가 7이면 8bit는 결정되고 1bit(종료bit)는 결정대기중 -> STOP_DIVISION으로 state이동
+        T_SAMPLE: begin //데이터 비트의 3번째 sample 저장 //8개의 data 데이터 & 1개의 stop bit가 들어 왔는지 확인
+            if(bit_cnt_reg == 4'd8) //bit_cnt = 8일 때 stop bit 확인 (9번째 bit)
                 n_state = STOP_DECISION;
-            else
+            else //bit_cnt = 7일 때까지 (0 ~ 7 -> 8bit): data bit 저장 상태로 이동 
                 n_state = DATA_DECISION;
         end
 
-        DATA_DECISION:
-            n_state = SAMPLE_CNT_RST;
+        DATA_DECISION: n_state = SAMPLE_CNT_RST; //3개의 sample bit 비교 후 register에 data bit 저장
 
-        STOP_DECISION: begin
+        STOP_DECISION: begin //stop bit 확인 후 rx complete or error 결정
             if(catch_bit == 1'b1)
                 n_state = RECEIVE_COMPLETE;
             else   
@@ -135,47 +133,9 @@ always @(state or i_rx_d or sampling or sample_cnt) begin //state, i_rx_d, sampl
     endcase
 end
 
-//sample bit
-always @(posedge clk) begin
-    if(state == IDLE) begin
-        b0 <= 0;
-        b1 <= 0;
-        b2 <= 0;
-    end else if (state == SAMPLE_CNT_RST) begin
-        b0 <= 0;
-        b1 <= 0;
-        b2 <= 0;
-    end else if(state == F_SAMPLE)
-        b0 <= i_rx_d;
-    else if(state == S_SAMPLE)
-        b1 <= i_rx_d;
-    else if(state == S_SAMPLE)
-        b2 <= i_rx_d;
-    else begin
-        b0 <= b0;
-        b1 <= b1;
-        b2 <= b2;
-    end
-end 
-
-assign catch_bit = b0&b1 | b0&b2 | b1&b2;
-
-//output logic
-always @(posedge clk or negedge rst_n) begin
-    if(~rst_n)
-        o_rx_d <= 8'b1111_1111;
-    else begin
-        if(state == DATA_DECISION)
-            o_rx_d[bit_cnt_reg] <= catch_bit;
-        else
-            o_rx_d <= o_rx_d;
-    end
-end
-
-assign o_rx_complete = state == RECEIVE_COMPLETE ? 1'b1 : 1'b0;
-assign o_rx_error = state == RECEIVE_ERROR ? 1'b1 : 1'b0;
-
 //Sample counter
+//sample_cnt = 0 일 때 1회 sampling 
+//sample_cnt = 1(sample_cnt = 0 이 끝나는 순간) 1번 check
 reg [4:0] sample_cnt;
 
 always @(posedge clk or negedge rst_n) begin
@@ -193,19 +153,62 @@ always @(posedge clk or negedge rst_n) begin
     end
 end
 
-//bit counter
-always @(posedge clk) begin
-    case(state)
-        IDLE:
-            bit_cnt_reg <= 4'd0;
-        
-        DATA_DECISION:
-            bit_cnt_reg <= bit_cnt_reg + 1'b1;
-        
-        default: 
-            bit_cnt_reg <= bit_cnt_reg;
+//Bit counter
+reg [3:0] bit_cnt;
 
-    endcase
+always @(posedge clk or negedge rst_n) begin
+    if(~rst_n)
+        bit_cnt <= 0;
+    else if(state == IDLE)
+        bit_cnt <= 0;
+    else if(state == DATA_DECISION)
+        bit_cnt <= bit_cnt + 1'b1;
+    else
+        bit_cnt <= bit_cnt;
 end
+
+//Sample bit register
+reg b0, b1, b2;
+
+always @(posedge clk or negedge rst_n) begin
+    if(~rst_n) begin
+        b0 <= 1'b0;
+        b1 <= 1'b0;
+        b2 <= 1'b0;
+    end else if(state == IDLE) begin
+        b0 <= 1'b0;
+        b1 <= 1'b0;
+        b2 <= 1'b0;
+    end else if(state == SAMPLE_CNT_RST) begin
+        b0 <= 1'b0;
+        b1 <= 1'b0;
+        b2 <= 1'b0;
+    end else if(state == F_SAMPLE) begin
+        b0 <= i_rx_d;
+    end else if(state == S_SAMPLE) begin
+        b1 <= i_rx_d;
+    end else if(state == T_SAMPLE) begin
+        b2 <= i_rx_d;
+    end else begin
+        b0 <= b0;
+        b1 <= b1;
+        b2 <= b2;
+    end
+end
+
+//Output logic
+always @(state) begin
+    if(state == DATA_DECISION) begin
+        catch_bit_cnt <= bit_cnt;
+        catch_bit <= (b0&b1) | (b0&b2) | (b1&b2); //Karnom map을 통하여 논리식 확인
+    end else begin
+        catch_bit_cnt <= catch_bit_cnt;
+        catch_bit <= catch_bit;
+    end
+end
+
+assign o_rx_complete = (state == RECEIVE_COMPLETE) ? 1'b1 : 1'b0;
+assign o_rx_error = (state == RECEIVE_ERROR) ? 1'b1 : 1'b0;
+
 
 endmodule
