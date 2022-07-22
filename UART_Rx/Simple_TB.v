497209859c7867df3a0fb6f763913749d8051841
kilee92 <82597294+kilee92@users.noreply.github.com>
2022-07-22 13:27:03 +0900

    Update Simple_TB.v

--- UART_Rx/Simple_TB.v
+++ UART_Rx/Simple_TB.v
@@ -30,7 +30,7 @@ initial `probe_start;
     `probe(sampling     );   
     `probe(catch_bit    );   
     `probe(catch_bit_cnt);   
-    `probe(shift_rst    );   
+    //`probe(shift_rst    );   
 
 //test
 initial begin
@@ -108,6 +108,13 @@ endmodule
 
 
 
+//작성자: 이광일
+//설계명: UART(Rx)
+//설계 목표: Shift Register를 포함하는 비동기 통신 중 하나인 UART Tx Module 설계 (Baud Rate = 115200)
+//참고:
+//- 데이터 전송 시점을 예측할 수 없기 때문에 1/Baud Rate보다 빠른 주기로 샘플링 하여 시작 비트가 수신되었는지 감지하는 기능을 포함해야함.
+//- 신호선의 무결성을 보장할 수 없으므로, 데이터 비트의 중간 지점 3부분에서 데이터를 샘플링하여 가장 많이 샘플링된 데이터 값을 저장해야함.
+
 module FSM_Module_Rx(
     clk                 ,
     rst_n               ,
@@ -160,9 +167,9 @@ always @(posedge clk or negedge rst_n) begin
 end
 
 //State logic
-always @(state or i_rx_d or sampling or sample_cnt) begin //state, i_rx_d, sampling 신호가 바뀔 때에만 동작(clk와 상관(X))
+always @(state or i_rx_d or sampling or sample_cnt or bit_cnt) begin //state, i_rx_d, sampling, sample_cnt, bit_cnt 신호가 바뀔 때에만 동작(clk와 상관(X))
     case(state)
-        IDLE: begin //sampling하여 시작 비트(0)를 감지하여 다음 state로 이동
+        IDLE: begin //sampling(baud rate의 16배 속도)하여 시작 비트(0)를 감지하여 다음 state로 이동
             if(sampling == 1 && i_rx_d == 0)
                 n_state = START_CHECK;
             else
@@ -170,62 +177,63 @@ always @(state or i_rx_d or sampling or sample_cnt) begin //state, i_rx_d, sampl
         end
 
         START_CHECK: begin
-            if(sampling == 1 && i_rx_d == 1) //sampling 하는 시작 비트(0)가 1이 될 경우(Noise가 낄 경우) 다시 IDLE로 상태로 돌아가 새로운 신호 전송을 기다림
+            if(sampling == 1 && i_rx_d == 1) //시작 비트(0) sampling 값이 1이 될 경우(Noise가 낄 경우) 다시 IDLE로 상태로 돌아가 새로운 신호 전송을 기다림
                 n_state = IDLE;
-            else if(sample_cnt == 3) //sample_cnt = 8(sample_cnt = 7 이 끝나는 순간)일 때 8번(0 ~ 7)을 check하고 다음 클락에 state 변경 -> middle point
+            else if(sample_cnt == 3) //start 비트의 middle point+1 (9번째 sampling)까지 대기
                 n_state = SAMPLE_CNT_RST;
             else
                 n_state = START_CHECK;
         end
 
-        SAMPLE_CNT_RST: n_state = F_WAIT; //sample_cnt 리셋
+        SAMPLE_CNT_RST: n_state = F_WAIT; //sample_cnt 리셋 -> 이전 data bit의 (middle point+1)일 때 Reset되므로, sample_cnt = 0 일 때 1번 sampling
 
-        F_WAIT: begin //시작 비트(0) middle point에서 부터 data bit (middle point - 2) 까지 
-            if(sample_cnt == 4) // sample_cnt는 sampling = 1일 때 1씩 증가
-                n_state = F_SAMPLE; // 다음 클럭에 state 변화 (다음 sampling이 아님)
+        F_WAIT: begin 
+            if(sample_cnt == 4) //data bit의 middle point-1 (이전 data bit의 middle point에서 부터 15번째 sampling) 까지 대기
+                n_state = F_SAMPLE; //sample_cnt = 14 이후 (1clock)
             else
                 n_state = F_WAIT;
         end
 
-        F_SAMPLE: n_state = S_WAIT; //데이터 비트의 1번째 sample 저장
+        F_SAMPLE: n_state = S_WAIT; //data bit의 1번째 sample 저장 (1clock) -> sample_cnt = 14 이후 (2clock)
 
-        S_WAIT: begin //시작 비트(0) middle point에서 부터 data bit (middle point - 1) 까지 
-            if(sample_cnt == 5) 
-                n_state = S_SAMPLE;
+
+        S_WAIT: begin
+            if(sample_cnt == 5) //data bit의 middle point(이전 data bit의 middle point에서 부터 16번째 sampling) 까지 대기
+                n_state = S_SAMPLE; //sample_cnt = 15 이후 (1clock)
             else
                 n_state = S_WAIT;
         end
 
-        S_SAMPLE: n_state = T_WAIT; //데이터 비트의 2번째 sample 저장
+        S_SAMPLE: n_state = T_WAIT; //data bit의 2번째 sample 저장 (1clock) -> sample_cnt = 15 이후 (2clock)
 
-        T_WAIT: begin //시작 비트(0) middle point에서 부터 data bit (middle point) 까지 
-            if(sample_cnt == 6) //sample_cnt = 16 일때 16번 check가 끝나는 시점 -> middle point
-                n_state = T_SAMPLE;
+        T_WAIT: begin 
+            if(sample_cnt == 6) //data bit의 middle point+1 (이전 data bit의 middle point에서 부터 17번째 sampling) 까지 대기
+                n_state = T_SAMPLE; //sample_cnt = 16 이후 (1clock)
             else
                 n_state = T_WAIT;
         end
 
-        T_SAMPLE: begin //데이터 비트의 3번째 sample 저장 //8개의 data 데이터 & 1개의 stop bit가 들어 왔는지 확인
-            if(bit_cnt == 4'd8) //bit_cnt = 8일 때 stop bit 확인 (9번째 bit)
+        T_SAMPLE: begin //data bit의 3번째 sample 저장 (1clock) -> sample_cnt = 16 이후 (2clock)
+            if(bit_cnt == 4'd8) //bit_cnt = 8일 때 stop bit까지 확인 (9번째 bit)
                 n_state = STOP_DECISION;
             else //bit_cnt = 7일 때까지 (0 ~ 7 -> 8bit): data bit 저장 상태로 이동 
                 n_state = DATA_DECISION;
         end
 
-        DATA_DECISION: n_state = SAMPLE_CNT_RST; //3개의 sample bit 비교 후 register에 data bit 저장
+        DATA_DECISION: n_state = SAMPLE_CNT_RST; //3개의 sample bit 비교 후 register에 data bit 저장 -> sample_cnt = 16 이후 (3clock)
 
-        STOP_DECISION: begin //stop bit 확인 후 rx complete or error 결정
+        STOP_DECISION: begin //Stop bit check (complete or error 결정) -> sample_cnt = 16 이후 (3clock)
             if(catch_bit == 1'b1)
                 n_state = RECEIVE_COMPLETE;
             else   
                 n_state = RECEIVE_ERROR;
         end
 
-        RECEIVE_COMPLETE:
+        RECEIVE_COMPLETE: //sample_cnt = 16 이후 (4clock)
             n_state = IDLE;
 
         RECEIVE_ERROR:
-            n_state = IDLE;
+            n_state = IDLE; //sample_cnt = 16 이후 (4clock)
 
         default: n_state = state;
     endcase
@@ -315,14 +323,26 @@ end
 assign o_rx_complete = (state == RECEIVE_COMPLETE) ? 1'b1 : 1'b0;
 assign o_rx_error = (state == RECEIVE_ERROR) ? 1'b1 : 1'b0;
 
-
 endmodule
 
 
 
 
 
-module Sampling_Counter(
+//작성자: 이광일
+//설계명: UART(Rx)
+//설계 목표: Shift Register를 포함하는 비동기 통신 중 하나인 UART Tx Module 설계 (Baud Rate = 115200)
+//참고:
+//- 데이터 전송 시점을 예측할 수 없기 때문에 1/Baud Rate보다 빠른 주기로 샘플링 하여 시작 비트가 수신되었는지 감지하는 기능을 포함해야함.
+//- 신호선의 무결성을 보장할 수 없으므로, 데이터 비트의 중간 지점 3부분에서 데이터를 샘플링하여 가장 많이 샘플링된 데이터 값을 저장해야함.
+
+module Sampling_Counter
+#(
+    parameter SYS_CLK   = 50000000  ,
+    parameter BAUD_RATE = 115200    ,
+    parameter DIVISION  = 16         
+)
+(
     clk         ,
     rst_n       ,
     sampling                        
