33d44643f08e551a87746962fb15a7b5a2326494
kilee92 <82597294+kilee92@users.noreply.github.com>
2022-07-25 23:20:05 +0900

    Update and rename Shift_Register.v to Shift_Register_Tx.v

--- UART_Tx/Shift_Register_Tx.v
+++ UART_Tx/Shift_Register_Tx.v
@@ -3,7 +3,7 @@
 //설계 목표: Shift Register를 포함하는 비동기 통신 중 하나인 UART Tx Module 설계
 //참고: Buad Rate은 115200(1/115200초 마다 1비트 전송)이며 시작비트(0)과 끝비트(1)을 포함하고 데이터비트는 8비트이다.
 
-module Shift_Register(
+module Shift_Register_Tx(
     clk         ,
     rst_n       ,
     i_tx_d      ,
