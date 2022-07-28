0ee75f03b2825aabf132b15cb1b3931e8ab23300
kilee92 <82597294+kilee92@users.noreply.github.com>
2022-07-28 15:07:33 +0900

    Update FSM_Module_Sobel.v

--- Sobel_Edge_Detector/FSM_Module_Sobel.v
+++ Sobel_Edge_Detector/FSM_Module_Sobel.v
@@ -32,7 +32,7 @@ module FSM_Module_Sobel
 input                       clk         ;
 input                       rst_n       ;
 input                       i_complete  ; //BRAM0에 1frame data 저장 완료
-input                       i_num_cnt   ; //BRAM0에 저장 되어 있는 1frame data의 Address Width
+input [ADDR_WIDTH-1:0]      i_num_cnt   ; //BRAM0에 저장 되어 있는 1frame data의 Address Width
 input                       i_run       ; //Sobel Filter 적용
 
 output [DATA_WIDTH-1:0]     b0_d1       ; //No use
@@ -86,7 +86,7 @@ wire read_done;
 wire write_done;
 
 always @(*) begin
-    case (state)
+    case (state_read)
         IDLE: begin 
             if(i_complete) begin
                 if(i_run)
@@ -118,7 +118,7 @@ always @(*) begin
 end
 
 always @(*) begin
-    case (state)
+    case (state_write)
         IDLE: begin 
             if(i_complete) begin
                 if(i_run)
@@ -153,13 +153,13 @@ end
 reg [ADDR_WIDTH-1:0] addr_cnt_read;
 reg [ADDR_WIDTH-1:0] addr_cnt_write;
 
-//Sobel Mask 적용을 위해 3*3 행렬의 행 순으로 data의 address 값에 접근하여 read
+//Sobel Mask 적용을 위해 3*3 행렬의 행 순으로 data address 값에 접근하여 read
 reg [1:0] col_cnt;
 
 always @(posedge clk or negedge rst_n) begin
     if(!rst_n)
         col_cnt <= 0;
-    else if(matrix_cnt == 2'd4) // 3clock(3*3행렬의 3열 3개의 data 접근 시간) + 2clock(임의로 설정) = 5clock(sobel filter total 계산 시간) -> Data를 Read하는데 소요하는 총 시간
+    else if(matrix_cnt == 2'd2) // 3clock(3*3행렬의 3열 3개의 data 접근 시간) + 2clock(임의로 설정) = 5clock(Data를 Read하는데 소요하는 총 시간) -> Pipeline 적용을 위해 Sobel mask 계산 시간(5clock - 임의로 설정)과 동일하게 맞춤
         col_cnt <= 0;
     else if(state == RUN)
         col_cnt <= col_cnt + 1;
@@ -176,14 +176,10 @@ always @(posedge clk or negedge rst_n) begin
         addr_cnt_read <= addr_cnt_read + 1;
     else if(state_read == RUN) begin
         if(col_cnt == 0)
-            addr_cnt_read <= addr_cnt_read + IMAGE_WIDTH; // Current Address 값은 3*3 행렬 중 (1,1) address, Next address 값은 3*3 행렬 중 (2,1) address
+            addr_cnt_read <= addr_cnt_read + IMAGE_WIDTH; // Current Address 값은 3*3 행렬 중 (1,1) address, Next address 값은 3*3 행렬 중 (1,1) address
         else if(col_cnt == 1)
-            addr_cnt_read <= addr_cnt_read + 2*IMAGE_WIDTH; // Current Address 값은 3*3 행렬 중 (2,1) address, Next address 값은 3*3 행렬 중 (3,1) address
-        else if(col_cnt == 2)
-            addr_cnt_read <= addr_cnt_read; // Current Address 값은 3*3 행렬 중 (3,1) address, Next address 값은 3*3 행렬 중 (3,1) address
-        else if(col_cnt == 3)
-            addr_cnt_read <= addr_cnt_read; // Current Address 값은 3*3 행렬 중 (3,1) address, Next address 값은 3*3 행렬 중 (3,1) address
-        else // col_cnt == 4
+            addr_cnt_read <= addr_cnt_read + 2*IMAGE_WIDTH; // Current Address 값은 3*3 행렬 중 (1,1) address, Next address 값은 3*3 행렬 중 (1,1) address
+        else // col_cnt == 2
             addr_cnt_read <= addr_cnt_read - 2*IMAGE_WIDTH + 1; // Current Address 값은 3*3 행렬 중 (3,1) address, Next address 값은 3*3 행렬 중 (1,2) address
     end else
         addr_cnt_read <= addr_cnt_read;
@@ -194,7 +190,7 @@ always @(posedge clk or negedge rst_n) begin
         addr_cnt_write <= 0;
     else if(state_write == DONE)
         addr_cnt_write <= 0;
-    else if(state_write == MOVE)
+    else if((state_write == MOVE) && b1_we1)
         addr_cnt_write <= addr_cnt_write + 1;
     else if((state_write == RUN) && b1_we1)
         addr_cnt_write <= addr_cnt_write + 1;
@@ -230,6 +226,7 @@ assign b0_we1      = 1'b0; //FSM은 BRAM0로 부터 읽기(Read)만 사용
 assign b0_addr1    = addr_cnt_read;
 
 //BRAM1 Output Logic
+
 //BRAM0에서 Read한 Data(b0_q1)는 1cycle 뒤에 나오므로 o_read보다 1clock 지연된 시점의 값을 얻어야함
 reg valid_read;
 
@@ -240,260 +237,110 @@ always @(posedge clk or negedge rst_n) begin
         valid_read <= o_read;
 end
 
-//BRAM0에서 3*4행렬 4열의 3개 data를 read하여 저장하는 단계
-reg [DATA_WIDTH-1:0] matrix_data [11:0];
-
-integer i;
-always @(posedge clk or negedge reset_n) begin
-    if(!rst_n) begin
-        matrix_data[0] <= {DATA_WIDTH{1'b0}};
-        matrix_data[1] <= {DATA_WIDTH{1'b0}};
-        matrix_data[2] <= {DATA_WIDTH{1'b0}};
-    end else if(valid_read) begin
-        for(i = 0; i < 3; i = i + 1)
-		    matrix_data[i] <= b0_q1; // read data
-	end
-end
-
-//BRAM0에서 3*4행렬 4열의 3개 data를 read하여 저장하기 위해 5clock 지연 시간 필요
-reg [4:0] proc_delay;
+//(MOVE) - BRAM에서 data caputre -> shift*4(delay) -> write (total 6cycle) (RUN시간과 동일하게 맞추기 위해)
+//(RUN) - BRAM0에서 data1 caputre -> data2 caputre -> data3 caputre -> data1, data2, data3 shift(delay) -> ALU -> write (total 6cycle)
+reg [5:0] move_core_delay;
+reg [5:0] run_core_delay; 
 
 always @(posedge clk or negedge rst_n) begin
     if(!rst_n)
-        proc_delay <= 0;
-    else if(proc_delay[4])
-        proc_delay <= {4'b0, valid_read};
-    else
-        proc_delay <= {proc_delay[3:0], valid_read};
+        move_core_delay <= 0;
+        run_core_delay <= 0;
+    else if(state == MOVE)
+        move_core_delay <= {move_core_delay[4:0], valid_read};
+    else if(state == RUN)
+        run_core_delay <= {run_core_delay[4:0], valid_read};
+    else begin
+        move_core_delay <= {move_core_delay[4:0], 0};
+        run_core_delay <= {move_core_delay[4:0], 0};
+    end
 end
 
-//5clock 이후 data shift (다음 data가 read 되기 전까지) -> proc_delay[4] = 1 일 때 4열->3열, 3열->2열, 2열->1열
-genvar  idx;
+//Read data capture
+//(MOVE) - BRAM0에서 read한 data를 shift register에 capture (1cycle)
+//(RUN) - BRAM0에서 read한 data(data1, data2, data3)를 shift register[0],[1],[2](3*4행렬의 4열)에 capture (1~3cycle)
+reg [DATA_WIDTH-1:0] move_data [5:0];
+reg [DATA_WIDTH-1:0] run_data [11:0];
+
+always @(posedge clk or negedge reset_n) begin
+    if(!rst_n) begin
+        move_data[0] <= {DATA_WIDTH{1'b0}};
+        run_data[0] <= {DATA_WIDTH{1'b0}};
+        run_data[1] <= {DATA_WIDTH{1'b0}};
+        run_data[2] <= {DATA_WIDTH{1'b0}};
+    end else if(valid_read) begin
+        if(state_read == MOVE)
+            move_data[0] <= b0_q1;
+        else if(state_read == RUN) begin
+		    run_data[0] <= b0_q1; // read data
+            run_data[1] <= run_data[0];
+            run_data[2] <= run_data[1];
+        end else;
+    end else begin
+        move_data[0] <= {DATA_WIDTH{1'b0}};
+        run_data[0] <= {DATA_WIDTH{1'b0}};
+        run_data[1] <= {DATA_WIDTH{1'b0}};
+        run_data[2] <= {DATA_WIDTH{1'b0}};
+    end
+end
+
+//Capture data shift
+//(MOVE) data shift (2~5cycle)
+//(RUN) 행열(4열->3열, 3열->2열, 2열->1열) shift (4cycle)
+genvar idx_move;
+genvar idx_run;
+
 generate
-    for (idx = 0; idx < 3; idx = idx + 1) begin :
+    for (idx_move = 0; idx_move < 5; idx_move = idx_move + 1) begin : gen_move_delay
         always @(posedge clk or negedge reset_n) begin
             if(!rst_n)
-                matrix_data[idx + 3] <= {DATA_WIDTH{1'b0}};
-                matrix_data[idx + 6] <= {DATA_WIDTH{1'b0}};
-                matrix_data[idx + 9] <= {DATA_WIDTH{1'b0}};
-            else if(proc_delay[4])
-                matrix_data[idx + 3] <= matrix_data[idx];
-                matrix_data[idx + 6] <= matrix_data[idx + 3];
-                matrix_data[idx + 9] <= matrix_data[idx + 6];
+                move_data[idx_move + 1] <= {DATA_WIDTH{1'b0}};
+            else if(|move_core_delay)
+                move_data[idx_move + 1] <= move_data[idx_move]
             else;
         end
     end
 endgenerate
 
-//5clock 동안 Sobel mask 계산 (다음 data가 shift 되기 전까지) -> proc_delay[4] = 1 일 때 o_sobel 캡처
-reg [7:0] result;
-wire [7:0] o_sobel
-
-always @(posedge clk or negedge rst_n) begin
-    if(!rst_n)
-        result <= 0;
-    else if(proc_delay[4])
-        result <= o_sobel;
-    else
-        result <= result;
-end
-
-
-//Write 동작은 3번째 Cycle(Read, Calculation)부터 동작해야 하므로 지연 시간 필요
-reg [5:0] init_delay;
-
-always @(posedge clk or negedge rst_n) begin
-    if(!rst_n)
-        init_delay <= 0;
-    else if(proc_delay[4])
-        init_delay <= {init_delay[4:0], proc_delay[4]};
-    else
-        init_delay <= init_delay;
-end
-
-
-assign b1_d1 = result;
-assign b1_ce1 = proc_delay[2] && init_delay[5];
-assign b1_we1 = proc_delay[2] && init_delay[5];
-assign b1_addr1 = addr_cnt_write;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////
-//store address count data (image size) at F/F
-reg [CNT_BIT-1:0] num_cnt;
-reg [CNT_BIT-1:0] r_num_cnt;
-always @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
-        num_cnt <= 0;
-        r_num_cnt <= 0;
-    end else if(i_run) begin
-        num_cnt <= i_num_cnt;
-        r_num_cnt <= r_i_num_cnt;
-    end else if (o_done) begin
-        num_cnt <= 0;
-        r_num_cnt <= 0;
-    end
-end
-
-reg [CNT_BIT-1:0] addr_cnt_read;
-reg [CNT_BIT-1:0] addr_cnt_write;
-assign is_done_read = o_read && (addr_cnt_read == num_cnt-1);
-assign is_done_write = o_write && (addr_cnt_write == r_num_cnt-1);
-
-always @(posedge clk or negedge rst_n) begin
-    if(!rst_n) begin
-        addr_cnt_read <= 0;
-    end else if(is_done_read) begin
-        addr_cnt_read <= 0;
-    end else if(o_read) begin
-        addr_cnt_read <= addr_cnt_read + 1;
-    end
-end
-
-always @(posedge clk or negedge rst_n) begin
-    if(!rst_n) begin
-        addr_cnt_write <= 0;
-    end else if(is_done_write) begin
-        addr_cnt_write <= 0;
-    end else if(o_write && we_b1) begin
-        addr_cnt_write <= addr_cnt_write + 1;
-    end
-end
-
-assign addr_b0 = addr_cnt_read;
-assign ce_b0 = o_read;
-assign we_b0 = 1'b0;
-assign d_b0 = {DATA_WIDTH{1'b0}};
-
-// 1 cycle delay o_read signal for using data from bram
-reg d_o_read;
-always @(posedge clk or negedge rst_n) begin
-    if(!rst_n) begin
-        d_o_read <= 0;  
-    end else begin
-        d_o_read <= o_read;
-    end
-end
-
-//Count image width & height of image
-reg [31:0] cnt_image_w;
-reg [31:0] cnt_image_h;
-always @(posedge clk or negedge rst_n) begin
-    if(!rst_n) begin
-        cnt_image_w <= 31'b0;
-        cnt_image_h <= 31'b0;
-    end else if (d_o_read && (cnt_image_w == IMAGE_W-1) && (cnt_image_h == IMAGE_H-1)) begin
-        cnt_image_h <= 31'b0;
-        cnt_image_w <= 31'b0;
-    end else if (d_o_read && (cnt_image_w == IMAGE_W-1)) begin
-        cnt_image_w <= 31'b0;
-        cnt_image_h <= cnt_image_h + 1;
-    end else if (d_o_read) begin
-        cnt_image_w <= cnt_image_w + 1;
-    end
-end
-
-// Store image pixel data at 3 row for applying sobel filter
-reg [7:0] r1 [0:IMAGE_W-1];
-reg [7:0] r2 [0:IMAGE_W-1];
-reg [7:0] r3 [0:IMAGE_W-1];
-integer i;
-always@(posedge clk or negedge rst_n) begin
-    if(!rst_n) begin
-        for(i=0; i<IMAGE_W; i=i+1) begin
-            r1[i] <= 8'b0;
-            r2[i] <= 8'b0;
-            r3[i] <= 8'b0;
+generate
+    for (idx_run = 0; idx_run < 3; idx_run = idx_run + 1) begin : gen_run_delay
+        always @(posedge clk or negedge reset_n) begin
+            if(!rst_n) begin
+                run_data[idx_run + 3] <= {DATA_WIDTH{1'b0}}; //3열
+                run_data[idx_run + 6] <= {DATA_WIDTH{1'b0}}; //2열
+                run_data[idx_run + 9] <= {DATA_WIDTH{1'b0}}; //1열
+            end else if(core_delay[3]) begin //001111(4cycle)
+                if(state_read == RUN)
+                run_data[idx_run + 3] <= matrix_data[idx_run];
+                run_data[idx_run + 6] <= matrix_data[idx_run + 3];
+                run_data[idx_run + 9] <= matrix_data[idx_run + 6];
+            end else;
         end
-    end else if(d_o_read) begin
-        r1[cnt_image_w] <= r2[cnt_image_w];        
-        r2[cnt_image_w] <= r3[cnt_image_w];
-        r3[cnt_image_w] <= q_b0;
     end
-end
+endgenerate
 
-// 1 cycle delay image address count for data stored at r1,r2,r3
-reg [31:0] d_cnt_image_w;
-reg [31:0] d_cnt_image_h;
-always @(posedge clk or negedge rst_n) begin
-    if(!rst_n) begin
-        d_cnt_image_w <= 32'b0;
-        d_cnt_image_h <= 32'b0;          
-    end else begin
-        d_cnt_image_w <= cnt_image_w;
-        d_cnt_image_h <= cnt_image_h;
-    end
-end
+//Shift 된 행열의 ALU
+//(RUN) 행열 shift 이후 4열을 제외한 3*3 행열 sobel mask 계산 (5cycle)
+wire [7:0] p0, p1, p2, p3, p4, p5, p6, p7, p8;
+wire [7:0] o_sobel;
 
-// 8 pixel start to be applied for sobel filter when third row and coloum data is stored at r1,r2,r3
-reg [7:0] p0, p1, p2, p3, p5, p6, p7, p8;
-reg result_valid;
-always @(posedge clk or negedge rst_n) begin
-    if(!rst_n) begin
-        result_valid = 0;
-        p0 <= 0;
-        p1 <= 0;
-        p2 <= 0;
-        p3 <= 0;
-        p5 <= 0;
-        p6 <= 0;
-        p7 <= 0;
-        p8 <= 0;
-    end else if((d_cnt_image_w >= 2) && (d_cnt_image_h >= 2)) begin
-        result_valid = 1;
-        p0 <= r1[d_cnt_image_w-2];
-        p1 <= r1[d_cnt_image_w-1];
-        p2 <= r1[d_cnt_image_w];
-        p3 <= r2[d_cnt_image_w-2];
-        p5 <= r2[d_cnt_image_w];
-        p6 <= r3[d_cnt_image_w-2];
-        p7 <= r3[d_cnt_image_w-1];
-        p8 <= r3[d_cnt_image_w];
-    end else begin
-        result_valid = 0;
-        p0 <= 0;
-        p1 <= 0;
-        p2 <= 0;
-        p3 <= 0;
-        p5 <= 0;
-        p6 <= 0;
-        p7 <= 0;
-        p8 <= 0;        
-    end
-end
+assign p0 = run_data[9];
+assign p1 = run_data[6];
+assign p2 = run_data[3];
+assign p3 = run_data[10];
+assign p4 = run_data[7];
+assign p5 = run_data[4];
+assign p6 = run_data[11];
+assign p7 = run_data[8];
+assign p8 = run_data[2];
 
-wire [7:0] result_sobel;
+//최종 data Write
+//(MOVE) 6cycle
+//(RUN) 6cycle
+assign b1_addr1 = addr_cnt_write;
+assign b1_ce1 = move_core_delay[5] || run_core_delay[5];
+assign b1_we1 = move_core_delay[5] || run_core_delay[5];
+assign b1_d1 = move_core_delay[5]*move_data[5] + run_core_delay[5]*o_sobel;
 
-sobel_filter
-#(
-)
-u_sobel_filter(
-    .p0(p0),
-    .p1(p1),
-    .p2(p2),
-    .p3(p3),
-    .p5(p5),
-    .p6(p6),
-    .p7(p7),
-    .p8(p8),
-    .o_sobel(result_sobel)
-);
-
-assign addr_b1 = addr_cnt_write;
-assign ce_b1 = result_valid;
-assign we_b1 = result_valid;
-assign d_b1 = result_sobel;
-
+//(RUN) 매 IMAGE_WIDTH 마다 첫 2번의 b1_d1 data는 유효하지 않음 (3*3행열의 2,3열 data만 채워져 있음) + IMAGE의 Edge 부분의 축소되므로 Resizing이 필요
 endmodule
