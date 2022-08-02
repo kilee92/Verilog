9bed0d05f4466d1539e32969760cc3191541b674
kilee92 <82597294+kilee92@users.noreply.github.com>
2022-08-02 15:30:10 +0900

    Update FSM_Module_Sobel.v

--- Sobel_Edge_Detector/FSM_Module_Sobel.v
+++ Sobel_Edge_Detector/FSM_Module_Sobel.v
@@ -2,10 +2,10 @@ module FSM_Module_Sobel
 #(
     parameter DATA_WIDTH    = 8,
     parameter ADDR_WIDTH    = 16,
-    parameter MEM_SIZE      = 65536, // 2^16
+    parameter MEM_SIZE      = 65536, //2^16
 
-    parameter IMAGE_WIDTH   = 100,
-    parameter IMAGE_HEIGHT  = 100
+    parameter IMAGE_WIDTH   = 5,
+    parameter IMAGE_HEIGHT  = 5
 )
 (
     clk         ,
@@ -28,6 +28,57 @@ module FSM_Module_Sobel
     o_write     ,
     o_done       
 );
+    
+    `probe(state_read);
+    `probe(state_write);
+    /*
+    `probe(addr_cnt_read);
+    `probe(b0_addr1);
+    `probe(b0_q1);
+    `probe(b1_addr1);
+    `probe(b1_d1);
+    `probe(o_read);
+    `probe(valid_read);
+    */
+    `probe(sobel_core_delay[0]);
+    `probe(sobel_core_delay[1]);
+    `probe(sobel_core_delay[2]);
+    `probe(b1_addr1);
+    `probe(b1_d1);
+    `probe(o_read);
+    `probe(valid_read);
+    `probe(addr_cnt_write);
+    `probe(b1_we1);
+    `probe(b1_ce1);
+    /*
+    `probe(move_core_delay);
+    `probe(sobel_core_delay[0]);
+    `probe(sobel_core_delay[1]);
+    `probe(sobel_core_delay[2]);
+    `probe(b1_we1);
+    `probe(addr_cnt_write);
+    `probe(b1_addr1);
+    `probe(b1_d1);
+    `probe(sobel_data[0]);
+    `probe(sobel_data[1]);
+    `probe(sobel_data[2]);
+    `probe(sobel_data[3]);
+    `probe(sobel_data[4]);
+    `probe(sobel_data[5]);
+    `probe(sobel_data[6]);
+    `probe(sobel_data[7]);
+    `probe(sobel_data[8]);
+    `probe(p0);
+    `probe(p1);
+    `probe(p2);
+    `probe(p3);
+    `probe(p4);
+    `probe(p5);
+    `probe(p6);
+    `probe(p7);
+    `probe(p8);
+    `probe(o_sobel);
+    */
 
 input                       clk         ;
 input                       rst_n       ;
@@ -45,7 +96,7 @@ input [DATA_WIDTH-1:0]      b0_q1       ;
 output [DATA_WIDTH-1:0]     b1_d1       ;
 output                      b1_ce1      ;
 output                      b1_we1      ;
-output [ADDR_WIDTH-1:0]     b1_addr1    ;
+output reg [ADDR_WIDTH-1:0] b1_addr1    ;
 
 input [DATA_WIDTH-1:0]      b1_q1       ; //No use
 
@@ -55,14 +106,17 @@ output                      o_write     ;
 output                      o_done      ;
 
 //Pipeline 적용을 위해 2가지(Read/Write) FSM 사용
-reg [1:0] state_read, n_state_read;
-reg [1:0] state_write, n_state_write;
+reg [2:0] state_read, n_state_read;
+reg [2:0] state_write, n_state_write;
 
 //State Parameter
-localparam IDLE     = 2'b00;
-localparam MOVE     = 2'b01; //Sobel Mask 적용 안하여 데이터 이동(단순 데이터 이동)
-localparam RUN      = 2'b10; //Sobel Mask 적용 하여 데이터 이동
-localparam DONE     = 2'b11;
+localparam IDLE             = 3'b000;
+localparam MOVE             = 3'b001; //Sobel Mask 적용 안하여 데이터 이동(단순 데이터 이동)
+localparam MOVE_DONE        = 3'b010;
+localparam SOBEL            = 3'b011; //Sobel Mask 적용 하여 데이터 이동
+localparam SOBEL_CNT_RST    = 3'b100;
+localparam SOBEL_RESIZE     = 3'b101;
+localparam SOBEL_DONE       = 3'b110;
 
 //State Register
 always @(posedge clk or negedge rst_n) begin
@@ -84,13 +138,14 @@ end
 //State Logic
 wire read_done;
 wire write_done;
+wire sobel_done;
 
-always @(*) begin
+    always @(*) begin
     case (state_read)
         IDLE: begin 
             if(i_en) begin
                 if(i_run)
-                    n_state_read = RUN;
+                    n_state_read = SOBEL;
                 else
                     n_state_read = MOVE;
             end else
@@ -99,21 +154,23 @@ always @(*) begin
 
         MOVE: begin
             if(read_done)
-                n_state_read = DONE;
+                n_state_read = MOVE_DONE;
             else   
                 n_state_read = MOVE;
         end
                 
-        RUN: begin
+        SOBEL: begin
             if(read_done)
-                n_state_read = DONE;
+                n_state_read = SOBEL_DONE;
             else   
-                n_state_read = RUN;
+                n_state_read = SOBEL;
         end
 
-        DONE: n_state_read = IDLE;
+        MOVE_DONE: n_state_read = IDLE;
+        SOBEL_DONE: n_state_read = IDLE;
 
         default: n_state_read = IDLE;
+        
     endcase
 end
 
@@ -122,7 +179,7 @@ always @(*) begin
         IDLE: begin 
             if(i_en) begin
                 if(i_run)
-                    n_state_write = RUN;
+                    n_state_write = SOBEL;
                 else
                     n_state_write = MOVE;
             end else
@@ -131,81 +188,38 @@ always @(*) begin
 
         MOVE: begin
             if(write_done)
-                n_state_write = DONE;
+                n_state_write = MOVE_DONE;
             else   
                 n_state_write = MOVE;
         end
                 
-        RUN: begin
-            if(write_done)
-                n_state_write = DONE;
+        SOBEL: begin
+            if(sobel_done)
+                n_state_write = SOBEL_CNT_RST;
             else   
-                n_state_write = RUN;
+                n_state_write = SOBEL;
         end
 
-        DONE: n_state_write = IDLE;
+        SOBEL_CNT_RST: 
+            n_state_write = SOBEL_RESIZE;
+
+        SOBEL_RESIZE: begin
+            if(write_done)
+                n_state_write = SOBEL_DONE;
+            else   
+                n_state_write = SOBEL_RESIZE;
+        end        
+
+        MOVE_DONE: n_state_write = IDLE;
+        SOBEL_DONE: n_state_write = IDLE;
 
         default: n_state_write = IDLE;
     endcase
 end
 
-//Address Counter
+/////////////////////////////////////////////////////READ Data from BRAM0//////////////////////////////////////////////////////////////
+
 reg [ADDR_WIDTH-1:0] addr_cnt_read;
-reg [ADDR_WIDTH-1:0] addr_cnt_write;
-reg [ADDR_WIDTH-1:0] addr_cnt_write_run;
-
-//Sobel Mask 적용을 위해 3*3 행렬의 행 순으로 data address 값에 접근하여 read
-reg [1:0] col_cnt;
-
-always @(posedge clk or negedge rst_n) begin
-    if(!rst_n)
-        col_cnt <= 0;
-    else if(col_cnt == 2'd2) //3행씩 접근
-        col_cnt <= 0;
-    else if(n_state_read == RUN)
-        col_cnt <= col_cnt + 1;
-    else
-        col_cnt <= 0;
-end
-
-always @(posedge clk or negedge rst_n) begin
-    if(!rst_n)
-        addr_cnt_read <= 0;
-    else if(state_read == DONE)
-        addr_cnt_read <= 0;
-    else if(state_read == MOVE)
-        addr_cnt_read <= addr_cnt_read + 1;
-    else if(state_read == RUN) begin
-        if(col_cnt == 0)
-            addr_cnt_read <= addr_cnt_read + IMAGE_WIDTH; // Current Address 값은 3*3 행렬 중 (1,1) address, Next address 값은 3*3 행렬 중 (1,1) address
-        else if(col_cnt == 1)
-            addr_cnt_read <= addr_cnt_read + 2*IMAGE_WIDTH; // Current Address 값은 3*3 행렬 중 (1,1) address, Next address 값은 3*3 행렬 중 (1,1) address
-        else // col_cnt == 2
-            addr_cnt_read <= addr_cnt_read - 2*IMAGE_WIDTH + 1; // Current Address 값은 3*3 행렬 중 (3,1) address, Next address 값은 3*3 행렬 중 (1,2) address
-    end else
-        addr_cnt_read <= addr_cnt_read;
-end
-
-always @(posedge clk or negedge rst_n) begin
-    if(!rst_n) begin
-        addr_cnt_write <= 0;
-        addr_cnt_write_run <= 0;
-    end else if(state_write == DONE) begin
-        addr_cnt_write <= 0;
-        addr_cnt_write_run <= 0;
-    end else if((state_write == MOVE) && b1_we1)
-        addr_cnt_write <= addr_cnt_write + 1;
-    else if((state_write == RUN) && b1_we1) begin //Sobel mask 적용시 매 IMAGE_WIDTH 마다 첫 2번의 b1_d1 data는 유효하지 않음 (3*3행열의 2,3열 data만 채워져 있음)
-        addr_cnt_write_run <= addr_cnt_write_run + 1;
-            if(addr_cnt_write_run % IMAGE_WIDTH == 0)
-                addr_cnt_write <= addr_cnt_write;
-            else if(addr_cnt_write_run % IMAGE_WIDTH == 1)
-                addr_cnt_write <= addr_cnt_write;
-            else
-                addr_cnt_write <= addr_cnt_write + 1;
-    end else
-        addr_cnt_write <= addr_cnt_write;
-end
 
 //Registering(Capture) count number
 reg [ADDR_WIDTH-1:0] num_cnt;
@@ -219,22 +233,43 @@ always @(posedge clk or negedge rst_n) begin
         num_cnt <= num_cnt;
 end
 
-//Output Logic
-assign read_done = (addr_cnt_read == num_cnt-1) && o_read;// o_read   = (state_read == MOVE) || (state_read == RUN)
-assign write_done = (addr_cnt_write == num_cnt-1) && o_write; // o_write  = (state_write == MOVE) || (state_write == RUN)
+//Sobel Mask 적용을 위해 3*3 행렬의 행 순으로 BRAM0에 접근하여 read하기 위한 counter
+reg [1:0] addr_cnt_read_check;
 
-assign o_idle   = (state_read == IDLE) && (state_write == IDLE);
-assign o_read   = (state_read == MOVE) || (state_read == RUN);
-assign o_write  = (state_write == MOVE) || (state_write == RUN);
-assign o_done   = state_write == DONE; //항상 write가 더 늦게 끝남
+always @(posedge clk or negedge rst_n) begin
+    if(!rst_n)
+        addr_cnt_read_check <= 0;
+    else if(addr_cnt_read_check == 2'd2) //3행씩 접근
+        addr_cnt_read_check <= 0;
+    else if(state_read == SOBEL)
+        addr_cnt_read_check <= addr_cnt_read_check + 1;
+    else
+        addr_cnt_read_check <= 0;
+end
+
+always @(posedge clk or negedge rst_n) begin
+    if(!rst_n)
+        addr_cnt_read <= 0;
+    else if(read_done)
+        addr_cnt_read <= 0;
+    else if(state_read == MOVE)
+        addr_cnt_read <= addr_cnt_read + 1;
+    else if(state_read == SOBEL) begin
+        if(addr_cnt_read_check == 2)
+            addr_cnt_read <= addr_cnt_read - 2*IMAGE_WIDTH + 1; // (3*3행렬) 3행까지 접근 후 1행부터 시작
+        else
+            addr_cnt_read <= addr_cnt_read + IMAGE_WIDTH; // SOBEL 적용을 위해 행순으로 data read
+    end else
+        addr_cnt_read <= addr_cnt_read;
+end
+
+assign read_done = (addr_cnt_read == num_cnt-1) && ((state_read == MOVE) || (state_read == SOBEL));
 
 //BRAM0 Output Logic
 assign b0_d1       = {DATA_WIDTH{1'b1}}; // No use
-assign b0_ce1      = o_read; // o_read   = (state_read == MOVE) || (state_read == RUN)
-assign b0_we1      = 1'b0; //FSM은 BRAM0로 부터 읽기(Read)만 사용
+assign b0_ce1      = (state_read == MOVE) || (state_read == SOBEL);
+assign b0_we1      = 1'b0; //BRAM0로 부터 읽기(Read)만 사용
 assign b0_addr1    = addr_cnt_read;
-
-//BRAM1 Output Logic
 
 //BRAM0에서 Read한 Data(b0_q1)는 1cycle 뒤에 나오므로 o_read보다 1clock 지연된 시점의 값을 얻어야함
 reg valid_read;
@@ -246,110 +281,131 @@ always @(posedge clk or negedge rst_n) begin
         valid_read <= o_read;
 end
 
-//(MOVE) - BRAM에서 data caputre -> shift*4(delay) -> write (total 6cycle) (RUN시간과 동일하게 맞추기 위해)
-//(RUN) - BRAM0에서 data1 caputre -> data2 caputre -> data3 caputre -> data1, data2, data3 shift(delay) -> ALU -> write (total 6cycle)
-reg [5:0] move_core_delay;
-reg [5:0] run_core_delay; 
+/////////////////////////////////////////////////////Write Data to BRAM1//////////////////////////////////////////////////////////////
+reg [ADDR_WIDTH-1:0] addr_cnt_write;
 
 always @(posedge clk or negedge rst_n) begin
-    if(!rst_n) begin
-        move_core_delay <= 0;
-        run_core_delay <= 0;
-    end else if(state_read == MOVE)
-        move_core_delay <= {move_core_delay[4:0], valid_read};
-    else if(state_read == RUN)
-        run_core_delay <= {run_core_delay[4:0], valid_read};
-    else begin
-        move_core_delay <= {move_core_delay[4:0], 1'b0};
-        run_core_delay <= {run_core_delay[4:0], 1'b0};
-    end
+    if(!rst_n)
+        addr_cnt_write <= 0;
+    else if(write_done)
+        addr_cnt_write <= 0;
+    else if(sobel_done) //Sobel mask 적용 후 image의 edge 부분의 data를 0으로 채우기(resize) 위해 address count값 초기화
+        addr_cnt_write <= 0;
+    else if(((state_write == MOVE) || (state_write == SOBEL)) && b1_we1)
+        addr_cnt_write <= addr_cnt_write + 1;
+    else if(state_write == SOBEL_RESIZE) begin
+        if((addr_cnt_write < IMAGE_WIDTH - 1) || (addr_cnt_write > (IMAGE_WIDTH * IMAGE_HEIGHT) - IMAGE_WIDTH - 1)) //image의 첫행과 마지막행 address에만 접근
+            addr_cnt_write <= addr_cnt_write + 1;
+        else begin //image의 1열과 마지막열 address에만 접근
+            if(addr_cnt_write % IMAGE_WIDTH == 0)
+                addr_cnt_write <= addr_cnt_write + (IMAGE_WIDTH - 1);
+            else  
+                addr_cnt_write = addr_cnt_write + 1;
+        end
+    end else
+        addr_cnt_write <= addr_cnt_write;
+end
+
+assign sobel_done = (addr_cnt_write == IMAGE_WIDTH * (IMAGE_HEIGHT-2) - 1) && sobel_core_delay[2] && (state_write == SOBEL);
+assign write_done = (addr_cnt_write == num_cnt - 1) && (state_write == MOVE || state_write == SOBEL_RESIZE);
+
+always @(*) begin
+    if(state_write == SOBEL)
+        if(addr_cnt_write % IMAGE_WIDTH >= 2) //1,2 번째 data는 유효하지 않음(3*3행이 모두 채워지지 않음)
+            b1_addr1 <= addr_cnt_write + IMAGE_WIDTH - 1; //Sobel mask 적용시 image의 edge 부분의 data가 없어지므로 image의 1번째 행에는 data를 채우지 않음
+        else
+            b1_addr1 <= addr_cnt_write;
+    else if((state_write == MOVE) || (state_write == SOBEL_RESIZE))
+        b1_addr1 <= addr_cnt_write;
 end
 
 //Read data capture
-//(MOVE) - BRAM0에서 read한 data를 shift register에 capture (0cycle)
-//(RUN) - BRAM0에서 read한 data(data1, data2, data3)를 shift register[0],[1],[2](3*4행렬의 4열)에 capture (0~2cycle)
-reg [DATA_WIDTH-1:0] move_data [5:0];
-reg [DATA_WIDTH-1:0] run_data [11:0];
+reg [DATA_WIDTH-1:0] move_data;
+reg [DATA_WIDTH-1:0] sobel_data [8:0];
 
 always @(posedge clk or negedge rst_n) begin
     if(!rst_n) begin
-        move_data[0] <= {DATA_WIDTH{1'b0}};
-        run_data[0] <= {DATA_WIDTH{1'b0}};
-        run_data[1] <= {DATA_WIDTH{1'b0}};
-        run_data[2] <= {DATA_WIDTH{1'b0}};
+        move_data <= {DATA_WIDTH{1'b0}};
+        sobel_data[0] <= {DATA_WIDTH{1'b0}};
+        sobel_data[1] <= {DATA_WIDTH{1'b0}};
+        sobel_data[2] <= {DATA_WIDTH{1'b0}};
     end else if(valid_read) begin
-        move_data[0] <= b0_q1;
-		run_data[0] <= b0_q1; // read data
-        run_data[1] <= run_data[0];
-        run_data[2] <= run_data[1];
+        if((state_read == MOVE) || (state_read == MOVE_DONE))
+            move_data <= b0_q1;
+        else if((state_read == SOBEL) || (state_read == SOBEL_DONE)) begin
+		    sobel_data[0] <= b0_q1;   
+            sobel_data[1] <= sobel_data[0];
+            sobel_data[2] <= sobel_data[1];
+        end
     end else begin
-        move_data[0] <= {DATA_WIDTH{1'b0}};
-        run_data[0] <= {DATA_WIDTH{1'b0}};
-        run_data[1] <= {DATA_WIDTH{1'b0}};
-        run_data[2] <= {DATA_WIDTH{1'b0}};
+        move_data <= {DATA_WIDTH{1'b0}};
+        sobel_data[0] <= {DATA_WIDTH{1'b0}};
+        sobel_data[1] <= {DATA_WIDTH{1'b0}};
+        sobel_data[2] <= {DATA_WIDTH{1'b0}};
     end
 end
 
-//Capture data shift
-//(MOVE) data shift (1~5cycle)
-//(RUN) 행열(4열->3열, 3열->2열, 2열->1열) shift (3cycle)
-genvar idx_move;
-genvar idx_run;
+//(SOBEL) 행렬을 채우기 위해 delay
+reg move_core_delay;
+reg [2:0] sobel_core_delay; 
 
-generate
-    for (idx_move = 0; idx_move < 5; idx_move = idx_move + 1) begin : gen_move_delay
-        always @(posedge clk or negedge rst_n) begin
-            if(!rst_n)
-                move_data[idx_move + 1] <= {DATA_WIDTH{1'b0}};
-            else if(|move_core_delay)
-                move_data[idx_move + 1] <= move_data[idx_move];
-            else;
-        end
+always @(posedge clk or negedge rst_n) begin
+    if(!rst_n)
+        sobel_core_delay <= 0;
+    else if(sobel_core_delay[2])
+        sobel_core_delay <= {2'b0, valid_read};
+    else if(valid_read) begin
+        if((state_read == MOVE) || (state_read == MOVE_DONE))
+            move_core_delay <= valid_read;
+        else if((state_read == SOBEL) || (state_read == SOBEL_DONE))
+            sobel_core_delay <= {sobel_core_delay[1:0], valid_read};
+    end else begin
+        move_core_delay <= 0;
+        sobel_core_delay <= 3'b0;
     end
-endgenerate
+end
+
+
+//(SOBEL) 행렬(3열->2열, 2열->1열) shift
+genvar idx;
 
 generate
-    for (idx_run = 0; idx_run < 3; idx_run = idx_run + 1) begin : gen_run_delay
+    for (idx = 0; idx < 3; idx = idx + 1) begin : gen_sobel_shift
         always @(posedge clk or negedge rst_n) begin
             if(!rst_n) begin
-                run_data[idx_run + 3] <= {DATA_WIDTH{1'b0}}; //3열
-                run_data[idx_run + 6] <= {DATA_WIDTH{1'b0}}; //2열
-                run_data[idx_run + 9] <= {DATA_WIDTH{1'b0}}; //1열
-            end else if(run_core_delay[3]) begin //001111(4cycle)
-                if(state_read == RUN) begin
-                run_data[idx_run + 3] <= run_data[idx_run];
-                run_data[idx_run + 6] <= run_data[idx_run + 3];
-                run_data[idx_run + 9] <= run_data[idx_run + 6];
-                end else;
+                sobel_data[idx + 3] <= {DATA_WIDTH{1'b0}}; 
+                sobel_data[idx + 6] <= {DATA_WIDTH{1'b0}}; 
+            end else if(sobel_core_delay[2]) begin
+                sobel_data[idx + 3] <= sobel_data[idx];
+                sobel_data[idx + 6] <= sobel_data[idx + 3];
             end else;
         end
     end
 endgenerate
 
-//Shift 된 행열의 ALU
-//(RUN) 행열 shift 이후 4열을 제외한 3*3 행열 sobel mask 계산 (4cycle)
+//(SOBEL) 4열을 제외한 3*3 행열 sobel mask ALU
 wire [7:0] p0, p1, p2, p3, p4, p5, p6, p7, p8;
 wire [7:0] o_sobel;
 
-assign p0 = run_data[9];
-assign p1 = run_data[6];
-assign p2 = run_data[3];
-assign p3 = run_data[10];
-assign p4 = run_data[7];
-assign p5 = run_data[4];
-assign p6 = run_data[11];
-assign p7 = run_data[8];
-assign p8 = run_data[2];
+assign p0 = sobel_data[8];
+assign p1 = sobel_data[5];
+assign p2 = sobel_data[2];
+assign p3 = sobel_data[7];
+assign p4 = sobel_data[4];
+assign p5 = sobel_data[1];
+assign p6 = sobel_data[6];
+assign p7 = sobel_data[3];
+assign p8 = sobel_data[0];
 
-//최종 data Write
-//(MOVE) 6cycle
-//(RUN) 6cycle
-assign b1_addr1 = addr_cnt_write;
-assign b1_ce1 = move_core_delay[5] || run_core_delay[5];
-assign b1_we1 = move_core_delay[5] || run_core_delay[5];
-assign b1_d1 = move_core_delay[5]*move_data[5] + run_core_delay[5]*o_sobel;
+assign b1_ce1 = move_core_delay || sobel_core_delay[0] || (state_write == SOBEL_RESIZE);
+assign b1_we1 = move_core_delay || sobel_core_delay[2] || (state_write == SOBEL_RESIZE);
+assign b1_d1 = move_core_delay*move_data + sobel_core_delay[2]*o_sobel;
 
-// IMAGE의 Edge 부분 축소 IMAGE의 SIZE는 (IMGAE_WIDTH - 2) * (IMAGE_HEIGHT - 2)
+assign o_idle   = (state_read == IDLE) && (state_write == IDLE);
+assign o_read   = (state_read == MOVE) || (state_read == SOBEL);
+assign o_write  = (state_write == MOVE) || (state_write == SOBEL) || (state_write == SOBEL_RESIZE);
+assign o_done   = (state_write == MOVE_DONE) || (state_write == SOBEL_DONE);
+
 
 Sobel_Mask u0(
     .p0      (p0     ),
@@ -362,4 +418,5 @@ Sobel_Mask u0(
     .p8      (p8     ),
     .o_sobel (o_sobel) 
 );
+
 endmodule
